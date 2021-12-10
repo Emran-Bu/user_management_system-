f2ce6463e979b5e0aff6ebab872db26a153f9e74
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-12-10 08:54:33 +0600

    all link add

--- index.php
+++ index.php
@@ -5,11 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- <link rel="stylesheet" href="assets/font_awesome-5/font-awesome5.15.1cssall.min.css"> -->
-    <!-- <link rel="stylesheet" href="assets/bootstrap-5/bootstrap.min.css"> -->
-    <title>Document</title>
+    <!-- bootstrap css links -->
+    <link rel="stylesheet" href="assets/css/bootstrap-5/bootstrap.min.css">
+
+    <!-- fontawesome css links -->
+    <link rel="stylesheet" href="assets/css/font_awesome-5/font-awesome5.15.1cssall.min.css">
+
+    <title>User | Systems</title>
 </head>
 <body>
     
+        
+
+
+    <!-- jQuery Links -->
+    <script type="" src="assets/js/jqueryPlugin/jquery-3.5.1.min.js"></script>
+
+    <!-- bootstrap js links -->
+    <script src="assets/js/bootstrap-5/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
