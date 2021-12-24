d013942001495d56863103056fbb689fba4d1ee9
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-12-24 22:32:47 +0600

    admin users page create

---
+++ admin/admin-users.php
@@ -0,0 +1,15 @@
+<?php require_once 'assets/php/admin-header.php'; ?>
+
+<div class="row">
+    <h1><?= basename($_SERVER['PHP_SELF']) ?></h1>
+</div>
+<!-- footer part -->
+                <!-- 2nd col -->
+            </div>
+            <!-- row div end -->
+        </div>
+        <!-- container div end -->
+    </div>
+    
+</body>
+</html>
