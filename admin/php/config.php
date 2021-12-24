910adb3373eca1b40b4fabaf4d73290e90611d15
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-12-24 18:00:25 +0600

    config file create

--- admin/php/config.php
+++ admin/php/config.php
@@ -1,21 +1,78 @@
 <?php
 
-     class Database{
-         private $dsn = "mysql:host=localhost; dbname=db_user_system";
-         private $dbuser = "root";
-         private $dbpass = "";
+    class Database
+    {
+        const USERNAME = 'sharminsumi09@gmail.com';
+        const PASSWORD = 'emranGmail009';
+        
+        private $dsn = "mysql:host=localhost; dbname=db_user_system";
+        private $dbuser = "root";
+        private $dbpass = "";
 
-         public $conn;
+        public $conn;
 
-         public function __construct()
-         {
-             try {
-                 $this->conn = new PDO($this->dsn, $this->dbuser, $this->dbpass);
-             } catch (PDOException $e) {
-                 echo 'Error : ' . $e->getMessage();
-             }
-             return $this->conn;
-         }
-     }
+        public function __construct()
+        {
+            try {
+                $this->conn = new PDO($this->dsn, $this->dbuser, $this->dbpass);
+            } catch (PDOException $e) {
+                echo "Error: " . $e->getMessage();
+            }
+
+            return $this->conn;
+        }
+
+        // check input
+        public function test_input($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+        // error success message alert
+        public function showMessage($type, $message)
+        {
+            return '<div class="text-center alert alert-'.$type.' alert-dismissible p-1">
+                <strong>'.$message.'</strong>
+                <button type="button" class="btn" data-bs-dismiss="alert" style="font-size: 20px; margin-bottom: 8px;">&times;</button>
+            </div>';
+        }
+
+        // Display time in ago
+        public function timeInAgo($timestamps)
+        {
+            date_default_timezone_set('Asia/Dhaka');
+
+            $timestamps = strtotime($timestamps) ? strtotime($timestamps) : $timestamps;
+
+            $time = time() - $timestamps;
+
+            switch($time){
+                // seconds
+                case $time <= 60:
+                    return 'Just Now!';
+                // minutes
+                case $time >= 60 && $time < 3600:
+                    return (round($time/60) == 1) ? 'a minute ago' : round($time/60).' minutes ago';
+                // hours
+                case $time >= 3600 && $time < 86400:
+                    return (round($time/3600 == 1) ? 'an hour ago' : round($time/3600).' hours ago');
+                // days
+                case $time >= 86400 && $time < 604800:
+                    return (round($time/86400) == 1)? 'a day ago' : round($time/86400).' days ago';
+                // weeks
+                case $time >= 604800 && $time < 2600640:
+                    return (round($time/604800) == 1)? 'a week ago' : round($time/604800).' weeks ago';
+                // Months
+                case $time >= 2600640 && $time < 31207680:
+                    return (round($time/2600640) == 1)? 'a month ago' : round($time/2600640).' months ago';
+                // years
+                case $time >= 31207680:
+                    return (round($time/31207680) == 1)? 'a year ago' : round($time/31207680).' years ago';
+            }
+        }
+    }
 
 ?>
